0ca145a670f4a77d47af822444b46e04b9fd850d
FacundoPonce <mrfacu125@gmail.com>
2021-05-16 12:24:54 -0300

    Created script for map generation.

---
+++ BomberMan - PonceFacundo/Assets/Scripts/CreateMap.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class CreateMap : MonoBehaviour
+{   
+    [SerializeField] public int maxWidth; 
+    [SerializeField] public int maxHeight;
+    [SerializeField] public Color colorFloor;
+
+    [SerializeField] public GameObject refFloor;
+
+    private GameObject[,] map;
+    private float scaleTileX;
+    private float scaleTileZ;
+    public void Start()
+    {
+        GameObject [,] mapAux = new GameObject[maxWidth , maxHeight];
+        map = mapAux;
+        scaleTileX = refFloor.transform.localScale.x;
+        scaleTileZ = refFloor.transform.localScale.z;
+
+        Vector3 initialPos = new Vector3(0.5f, -0.5f, 0.5f);
+
+        for (int i = 0; i < maxWidth; i++)
+        {
+            for (int j = 0; j < maxHeight; j++)
+            {
+                map[i,j] = Instantiate(refFloor, new Vector3(initialPos.x + (i * scaleTileX),-0.5f, initialPos.z + (j * scaleTileZ)), 
+                    Quaternion.identity, transform);
+                //map[i,j].GetComponent<MeshRenderer>().material.color = colorFloor;
+            }
+        }
+    }
+}
