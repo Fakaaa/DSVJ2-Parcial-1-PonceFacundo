b174b5ec1921fb86586fd7425a5abdde96661e45
FacundoPonce <mrfacu125@gmail.com>
2021-05-16 21:07:00 -0300

    Trying to fix map generation when change matrix size.

--- BomberMan - PonceFacundo/Assets/Scripts/CreateMap.cs
+++ BomberMan - PonceFacundo/Assets/Scripts/CreateMap.cs
@@ -2,64 +2,59 @@
 
 public class CreateMap : MonoBehaviour
 {   
-    [SerializeField] public int maxWidth; 
-    [SerializeField] public int maxHeight;
+    [SerializeField][Range(20,32)] public int maxWidth; 
+    [SerializeField][Range(20,32)] public int maxHeight;
 
     [SerializeField] public GameObject refFloor;
     [SerializeField] public GameObject refWallBreakable;
     [SerializeField] public GameObject refWallUnBreakable;
 
-    private GameObject[,] floor;
-    private GameObject[,] walls;
-    private float scaleTileX;
-    private float scaleTileZ;
+    private GameObject floor;
+    private GameObject[,] wallsCenter;
+    private GameObject[] wallsOuterMap;
+    private float scaleTileXWalls;
+    private float scaleTileZWalls;
+
+    private float scaleFloorX;
+    private float scaleFloorY;
     public void Start()
     {
-        GameObject [,] mapAux = new GameObject[maxWidth , maxHeight];
-        floor = mapAux;
-        walls = mapAux;
-        scaleTileX = refFloor.transform.localScale.x;
-        scaleTileZ = refFloor.transform.localScale.z;
+        wallsCenter = new GameObject[maxWidth , maxHeight];
+        wallsOuterMap = new GameObject[4];
+        scaleTileXWalls = refWallUnBreakable.transform.localScale.x;
+        scaleTileZWalls = refWallUnBreakable.transform.localScale.z;
+        scaleFloorX = maxWidth;
+        scaleFloorY = maxHeight;
 
         Vector3 initialPos = new Vector3(0.5f, -0.5f, 0.5f);
 
-        for (int i = 0; i < maxWidth; i++)
+        wallsOuterMap[0] = Instantiate(refWallUnBreakable, new Vector3(scaleFloorX * 0.5f, 0.5f, 0.5f), refFloor.transform.localRotation, transform);
+        wallsOuterMap[0].transform.localScale = new Vector3(scaleFloorX, 1 , 1);
+
+        wallsOuterMap[1] = Instantiate(refWallUnBreakable, new Vector3(scaleFloorX * 0.5f, 0.5f, scaleFloorY - 0.5f), refFloor.transform.localRotation, transform);
+        wallsOuterMap[1].transform.localScale = new Vector3(scaleFloorX, 1, 1);
+
+        wallsOuterMap[2] = Instantiate(refWallUnBreakable, new Vector3(0.5f, 0.5f,scaleFloorY * 0.5f), refFloor.transform.localRotation, transform);
+        wallsOuterMap[2].transform.localScale = new Vector3(1, scaleFloorY, 1);
+
+        wallsOuterMap[3] = Instantiate(refWallUnBreakable, new Vector3(scaleFloorX - 0.5f, 0.5f, scaleFloorY * 0.5f), refFloor.transform.localRotation, transform);
+        wallsOuterMap[3].transform.localScale = new Vector3(1, scaleFloorY, 1);
+
+        for (int i = 1; i < maxWidth / 2.25f; i++)
         {
-            walls[i,0] = Instantiate(refWallUnBreakable, new Vector3(initialPos.x + (i * scaleTileX), 0.5f, initialPos.z + (0 * scaleTileZ)),
-                    refFloor.transform.localRotation, transform);
-            for (int j = 0; j < maxHeight; j++)
+            for (int j = 1; j < maxHeight / 2.25f; j++)
             {
-                walls[0, j] = Instantiate(refWallUnBreakable, new Vector3(initialPos.x + (0 * scaleTileX), 0.5f, initialPos.z + (j * scaleTileZ)),
-                    refFloor.transform.localRotation, transform);
-            }
-        }
-        for (int i = 0; i < maxWidth; i++)
-        {
-            walls[i, maxHeight-1] = Instantiate(refWallUnBreakable, new Vector3(initialPos.x + (i * scaleTileX), 0.5f, initialPos.z + (maxHeight - 1 * scaleTileZ)),
-                    refFloor.transform.localRotation, transform);
-            for (int j = 0; j < maxHeight; j++)
-            {
-                walls[maxWidth-1, j] = Instantiate(refWallUnBreakable, new Vector3(initialPos.x + (maxWidth - 1 * scaleTileX), 0.5f, initialPos.z + (j * scaleTileZ)),
-                    refFloor.transform.localRotation, transform);
+                Vector3 posWall = new Vector3((initialPos.x + (i * scaleTileXWalls)) * 2, 0.5f, (initialPos.z + (j * scaleTileZWalls)) * 2);
+                wallsCenter[i, j] = Instantiate(refWallUnBreakable, posWall , refFloor.transform.localRotation, transform);
             }
         }
 
-        for (int i = 1; i < maxWidth * 0.4f; i++)
-        {
-            for (int j = 1; j < maxHeight * 0.4f; j++)
-            {
-                walls[i, j] = Instantiate(refWallUnBreakable, new Vector3(initialPos.x + (i * (scaleTileX*2.1f)), 0.5f, initialPos.z + (j * (scaleTileZ* 2.1f))),
-                    refFloor.transform.localRotation, transform);
-            }
-        }
+        floor = Instantiate(refFloor, new Vector3( scaleFloorX * 0.5f, 0.0f ,scaleFloorY * 0.5f), refFloor.transform.localRotation, transform);
+        floor.transform.localScale = new Vector3(scaleFloorX, scaleFloorY, 1);
+    }
 
-        for (int i = 0; i < maxWidth; i++)
-        {
-            for (int j = 0; j < maxHeight; j++)
-            {
-                floor[i,j] = Instantiate(refFloor, new Vector3(initialPos.x + (i * scaleTileX), 0.0f, initialPos.z + (j * scaleTileZ)),
-                    refFloor.transform.localRotation, transform);
-            }
-        }
+    void FindPlaceWallsBreakable()
+    {
+
     }
 }
