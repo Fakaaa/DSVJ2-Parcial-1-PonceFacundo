74b16aee2ea5c3705083096f4e9141b2a87b91a1
FacundoPonce <mrfacu125@gmail.com>
2021-05-18 17:21:41 -0300

    Placed some fancy rotations in the menu.

---
+++ BomberMan - PonceFacundo/Assets/Scripts/AnimationMenu.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationMenu : MonoBehaviour
+{
+    [System.Serializable]
+    public enum TypeAnim
+    {
+        BarrelRolllllX,
+        BarrelRolllllY,
+        BarrelRolllllZ,
+    }
+    public TypeAnim myAnim;
+
+    [SerializeField] public float speedRotation;
+
+    void Update()
+    {
+        switch (myAnim)
+        {
+            case TypeAnim.BarrelRolllllX:
+                DoBarrelRollll(Vector3.right);
+                break;
+            case TypeAnim.BarrelRolllllY:
+                DoBarrelRollll(Vector3.up);
+                break;
+            case TypeAnim.BarrelRolllllZ:
+                DoBarrelRollll(Vector3.forward);
+                break;
+        }
+    }
+    public void DoBarrelRollll(Vector3 direction)
+    {
+        gameObject.transform.Rotate(direction, speedRotation * Time.deltaTime, Space.World);
+    }
+}
